b1cb1c603eef1a3b88ab2c6783201154416dedca
almostdead1 <126301945+almostdead1@users.noreply.github.com>
2023-11-23 06:28:09 +0530

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=Perf Kernel  Thanks to @ shivatejapeddi and CafEx
+kernel.string=Perf Kernel Thanks to @ Dhruvgera
 do.devicecheck=1
 do.modules=0
 do.systemless=1
